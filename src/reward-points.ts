862fe2a204123e99abc3020de170ae67ecfb2d86
lim0n3 <simonestaffa96@gmail.com>
2023-11-30 18:20:55 +0100

    farcaster api

---
+++ src/reward-points.ts
@@ -0,0 +1,54 @@
+interface Cast {
+  publishedAt: number;
+  username: string;
+  data: {
+    text: string;
+    image: string | null;
+    replyParentMerkleRoot: string | null;
+    threadMerkleRoot: string;
+  };
+}
+
+export const getCasts = async (
+  query: string,
+  after?: number,
+  options: { page?: number; count?: number } = { page: 0, count: 10 }
+): Promise<Cast[]> => {
+  let page = options.page || 0;
+  const { count } = options;
+  const searchCasterBaseUrl = new URL('https://searchcaster.xyz/api/search');
+  if (query) {
+    searchCasterBaseUrl.searchParams.append('text', query);
+  }
+  if (after) {
+    searchCasterBaseUrl.searchParams.append('after', after.toString());
+  }
+  if (page) {
+    searchCasterBaseUrl.searchParams.append('page', page.toString());
+  }
+  if (count) {
+    searchCasterBaseUrl.searchParams.append('count', count.toString());
+  }
+  const castsData: Cast[] = [];
+
+  let shouldStop = false;
+  while (!shouldStop) {
+    try {
+      // eslint-disable-next-line no-await-in-loop
+      const response = await fetch(searchCasterBaseUrl.toString());
+      // eslint-disable-next-line no-await-in-loop
+      const data = await response.json();
+      const { count: newCount, casts } = data.meta;
+      castsData.push(...casts);
+      if (newCount < count) {
+        shouldStop = true;
+      }
+    } catch (e) {
+      console.error(e);
+      break;
+    }
+    page++;
+    searchCasterBaseUrl.searchParams.set('page', page.toString());
+  }
+  return castsData;
+};
